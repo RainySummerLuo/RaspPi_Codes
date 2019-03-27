29b18939520ef81ced883c17439599c61c7bbeaf
Zichen Luo <iluo@163.com>
2019-03-27 10:38:10 +0800

    Update fan.py

--- fan.py
+++ fan.py
@@ -1,14 +1,12 @@
 import gpiozero
 
-RELAY_PIN = 7
+RELAY_PIN = 4
 
 fan = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
-led = FAN(7)
 
-if __name__ == "__main__":
-    if fan.value:
-        print("Switching Fan: OFF")
-        fan.off()
-    else:
-        print("Switching Fan: ON")
-        fan.on()
+if fan.value:
+    print("Switching Fan: OFF" + fan.value)
+    fan.off()
+else:
+    print("Switching Fan: ON")
+    fan.on()
